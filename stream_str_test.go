02758878d48d30b2556284f9346fe310478693c4
Aleksandr Razumov <ernado@ya.ru>
2021-10-28 22:47:26 +0300

    test: improve String tst coverage

--- stream_str_test.go
+++ stream_str_test.go
@@ -14,3 +14,17 @@ func TestStream_WriteStringWithHTMLEscaped(t *testing.T) {
 	const expected = `"\u003chtml\u003eHello\\\\\\n\\r\\\\\n\rWorld\u2028\u003c/html\u003e"`
 	require.Equal(t, expected, string(s.Buffer()))
 }
+
+func TestStream_WriteString(t *testing.T) {
+	s := NewStream(Default, nil, 0)
+	const data = `\nH\tel\tl\ro\\World\r` + "\n\rHello\r\tHi"
+	s.WriteString(data)
+	require.NoError(t, s.Error)
+	const expected = `"\\nH\\tel\\tl\\ro\\\\World\\r\n\rHello\r\tHi"`
+	require.Equal(t, expected, string(s.Buffer()))
+	t.Run("Read", func(t *testing.T) {
+		i := NewIterator(Default)
+		i.ResetBytes(s.Buffer())
+		require.Equal(t, data, i.ReadString())
+	})
+}
