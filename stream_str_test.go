734b1dcdedcc57a285e613dc3c9a673e3e82588c
Aleksandr Razumov <ernado@ya.ru>
2021-10-28 22:42:15 +0300

    test: improve html test

--- stream_str_test.go
+++ stream_str_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestStream_WriteStringWithHTMLEscaped(t *testing.T) {
 	s := NewStream(Default, nil, 0)
-	const data = `<html>Привет, мир!</html>`
+	const data = `<html>Hello\\\n\r\\` + "\n\rWorld\u2028</html>"
 	s.WriteStringWithHTMLEscaped(data)
 	require.NoError(t, s.Error)
-	const expected = `"\u003chtml\u003eПривет, мир!\u003c/html\u003e"`
+	const expected = `"\u003chtml\u003eHello\\\\\\n\\r\\\\\n\rWorld\u2028\u003c/html\u003e"`
 	require.Equal(t, expected, string(s.Buffer()))
 }
