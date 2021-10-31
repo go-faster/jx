ad81f704ad0a6438b8dc2106b0e60016ae8b8732
Aleksandr Razumov <ernado@ya.ru>
2021-10-31 14:34:22 +0300

    test: check pools concurrently

--- jx_test.go
+++ jx_test.go
@@ -1,9 +1,12 @@
 package jx
 
 import (
+	"bytes"
 	"encoding/json"
+	"sync"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,4 +16,42 @@ func requireCompat(t testing.TB, exp []byte, v interface{}) {
 	buf, err := json.Marshal(v)
 	require.NoError(t, err)
 	require.Equal(t, exp, buf)
+}
+
+func TestPutEncoder(t *testing.T) {
+	var wg sync.WaitGroup
+	for j := 0; j < 4; j++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 1024; i++ {
+				e := GetEncoder()
+				e.Raw("false")
+				assert.Equal(t, "false", e.String())
+				PutEncoder(e)
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func TestPutDecoder(t *testing.T) {
+	var wg sync.WaitGroup
+	for j := 0; j < 4; j++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 1024; i++ {
+				d := GetDecoder()
+				assert.Equal(t, d.Next(), Invalid)
+				d.Reset(bytes.NewBufferString("false"))
+				v, err := d.Bool()
+				assert.NoError(t, err)
+				assert.Equal(t, d.Next(), Invalid)
+				assert.False(t, v)
+				PutDecoder(d)
+			}
+		}()
+	}
+	wg.Wait()
 }
