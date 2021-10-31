e621df17ccb5b772bda70d44ec25ec548625a0a2
Aleksandr Razumov <ernado@ya.ru>
2021-10-31 12:46:54 +0300

    test(dec.arr): cover Decode_Arr

--- dec_arr_test.go
+++ dec_arr_test.go
@@ -10,6 +10,25 @@ import (
 func TestDecoder_Arr(t *testing.T) {
 	t.Run("Blank", func(t *testing.T) {
 		d := DecodeStr(`[]`)
+		require.NoError(t, d.Arr(nil))
+	})
+	t.Run("Invalid", func(t *testing.T) {
+		d := DecodeStr(`{`)
+		require.Error(t, d.Arr(nil))
+	})
+	t.Run("ErrUnexpectedEOF", func(t *testing.T) {
+		d := DecodeStr("")
+		require.ErrorIs(t, d.Arr(nil), io.ErrUnexpectedEOF)
+	})
+	t.Run("ErrUnexpectedEOF", func(t *testing.T) {
+		d := DecodeStr("[")
+		require.ErrorIs(t, d.Arr(nil), io.ErrUnexpectedEOF)
+	})
+}
+
+func TestDecoder_Elem(t *testing.T) {
+	t.Run("Blank", func(t *testing.T) {
+		d := DecodeStr(`[]`)
 		ok, err := d.Elem()
 		require.NoError(t, err)
 		require.False(t, ok)
