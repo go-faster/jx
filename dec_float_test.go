bcb8f76d56b75eabbc80736485ed376712278dce
Aleksandr Razumov <ernado@ya.ru>
2021-11-02 15:37:33 +0300

    test(dec.float): refactor decoder cases into decodeStr

--- dec_float_test.go
+++ dec_float_test.go
@@ -7,6 +7,37 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func decodeStr(t *testing.T, s string, f func(d *Decoder)) {
+	t.Helper()
+	for _, d := range []struct {
+		Name string
+		Fn   func() *Decoder
+	}{
+		{
+			Name: "DecodeStr",
+			Fn:   func() *Decoder { return DecodeStr(s) },
+		},
+		{
+			Name: "DecodeBytes",
+			Fn:   func() *Decoder { return DecodeBytes([]byte(s)) },
+		},
+		{
+			Name: "Decode",
+			Fn:   func() *Decoder { return Decode(bytes.NewBufferString(s), 0) },
+		},
+		{
+			Name: "DecodeSingleByteBuf",
+			Fn:   func() *Decoder { return Decode(bytes.NewBufferString(s), 1) },
+		},
+	} {
+		t.Run(d.Name, func(t *testing.T) {
+			t.Helper()
+			dec := d.Fn()
+			f(dec)
+		})
+	}
+}
+
 func TestDecoder_Float(t *testing.T) {
 	t.Run("Invalid", func(t *testing.T) {
 		for _, s := range []string{
@@ -20,24 +51,18 @@ func TestDecoder_Float(t *testing.T) {
 			`00.1`,
 		} {
 			t.Run(s, func(t *testing.T) {
-				for _, f := range []func() *Decoder{
-					func() *Decoder { return DecodeStr(s) },
-					func() *Decoder { return DecodeBytes([]byte(s)) },
-					func() *Decoder { return Decode(bytes.NewBufferString(s), defaultBuf) },
-					func() *Decoder { return Decode(bytes.NewBufferString(s), 1) },
-					func() *Decoder { return Decode(bytes.NewBufferString(s), 0) },
-				} {
-					t.Run("64", func(t *testing.T) {
-						d := f()
+				t.Run("64", func(t *testing.T) {
+					decodeStr(t, s, func(d *Decoder) {
 						_, err := d.Float64()
 						require.Error(t, err, s)
 					})
-					t.Run("32", func(t *testing.T) {
-						d := f()
+				})
+				t.Run("32", func(t *testing.T) {
+					decodeStr(t, s, func(d *Decoder) {
 						_, err := d.Float32()
 						require.Error(t, err, s)
 					})
-				}
+				})
 			})
 		}
 	})
