08ae3caab27b8c20fd37dcff8639f42337c527c5
Aleksandr Razumov <ernado@ya.ru>
2021-11-02 23:49:28 +0300

    chore: simplify package doc

--- jx.go
+++ jx.go
@@ -1,7 +1,4 @@
 // Package jx implements encoding and decoding of json as per RFC 4627.
-//
-// The Decoder provides a way to iterate over bytes/string/reader
-// and yield parsed elements one by one, fast.
 package jx
 
 import (
