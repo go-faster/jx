1f79ade84838158b0075ee3486d13a841738450b
Aleksandr Razumov <ernado@ya.ru>
2021-11-02 14:36:58 +0300

    test(bench): add BenchmarkValid

--- bench_test.go
+++ bench_test.go
@@ -24,6 +24,17 @@ func Benchmark_large_file(b *testing.B) {
 	}
 }
 
+func BenchmarkValid(b *testing.B) {
+	b.ReportAllocs()
+	b.SetBytes(int64(len(data)))
+
+	for n := 0; n < b.N; n++ {
+		if !Valid(data) {
+			b.Fatal("invalid")
+		}
+	}
+}
+
 func Benchmark_std_large_file(b *testing.B) {
 	b.ReportAllocs()
 	for n := 0; n < b.N; n++ {
