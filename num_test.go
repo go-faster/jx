e2daf50fd9bd5621ea1d2cf08a79806a8ac574d3
Aleksandr Razumov <ernado@ya.ru>
2021-11-04 18:01:42 +0300

    test(num): add Integer test

--- num_test.go
+++ num_test.go
@@ -14,3 +14,25 @@ func TestEncoder_Num(t *testing.T) {
 	})
 	require.Equal(t, e.String(), "123")
 }
+
+func TestNum(t *testing.T) {
+	t.Run("Integer", func(t *testing.T) {
+		v := Num{
+			Format: NumFormatInt,
+			Value:  []byte{'1', '2', '3'},
+		}
+		t.Run("Encode", func(t *testing.T) {
+			var e Encoder
+			e.Num(v)
+			require.Equal(t, e.String(), "123")
+		})
+		t.Run("Methods", func(t *testing.T) {
+			require.True(t, v.Positive())
+			require.True(t, v.Format.Int())
+			require.False(t, v.Format.Invalid())
+			require.False(t, v.Negative())
+			require.False(t, v.Zero())
+			require.Equal(t, 1, v.Sign())
+		})
+	})
+}
