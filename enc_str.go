85e24641a65589882b7e7bea3f6fefe890ddca25
tdakkota <tanc13@yandex.ru>
2022-04-05 13:42:59 +0300

    feat: add byte slice as JSON string writer

--- enc_str.go
+++ enc_str.go
@@ -6,6 +6,12 @@ func (e *Encoder) StrEscape(v string) {
 	e.w.StrEscape(v)
 }
 
+// ByteStrEscape encodes string with html special characters escaping.
+func (e *Encoder) ByteStrEscape(v []byte) {
+	e.comma()
+	e.w.ByteStrEscape(v)
+}
+
 // Str encodes string without html escaping.
 //
 // Use StrEscape to escape html, this is default for encoding/json and
@@ -13,4 +19,13 @@ func (e *Encoder) StrEscape(v string) {
 func (e *Encoder) Str(v string) {
 	e.comma()
 	e.w.Str(v)
+}
+
+// ByteStr encodes byte slice without html escaping.
+//
+// Use ByteStrEscape to escape html, this is default for encoding/json and
+// should be used by default for untrusted strings.
+func (e *Encoder) ByteStr(v []byte) {
+	e.comma()
+	e.w.ByteStr(v)
 }
--- w_str.go
+++ w_str.go
@@ -114,26 +114,34 @@ const hexChars = "0123456789abcdef"
 
 // StrEscape encodes string with html special characters escaping.
 func (w *Writer) StrEscape(v string) {
+	strEscape(w, v)
+}
+
+// ByteStrEscape encodes string with html special characters escaping.
+func (w *Writer) ByteStrEscape(v []byte) {
+	strEscape(w, v)
+}
+
+func strEscape[T byteseq](w *Writer, v T) {
 	length := len(v)
 	w.Buf = append(w.Buf, '"')
 	// Fast path, probably does not require escaping.
 	i := 0
 	for ; i < length; i++ {
 		c := v[i]
-		if c < utf8.RuneSelf && htmlSafeSet[c] {
-			w.Buf = append(w.Buf, c)
-		} else {
+		if c >= utf8.RuneSelf || !(htmlSafeSet[c]) {
 			break
 		}
 	}
+	w.Buf = append(w.Buf, v[:i]...)
 	if i == length {
 		w.Buf = append(w.Buf, '"')
 		return
 	}
-	w.strEscape(i, v, length)
+	strEscapeSlow[T](w, i, v, length)
 }
 
-func (w *Writer) strEscape(i int, v string, valLen int) {
+func strEscapeSlow[T byteseq](w *Writer, i int, v T, valLen int) {
 	start := i
 	// for the remaining parts, we process them char by char
 	for i < valLen {
@@ -143,7 +151,7 @@ func (w *Writer) strEscape(i int, v string, valLen int) {
 				continue
 			}
 			if start < i {
-				w.rawStr(v[start:i])
+				w.Buf = append(w.Buf, v[start:i]...)
 			}
 			switch b {
 			case '\\', '"':
@@ -167,10 +175,10 @@ func (w *Writer) strEscape(i int, v string, valLen int) {
 			start = i
 			continue
 		}
-		c, size := utf8.DecodeRuneInString(v[i:])
+		c, size := decodeRuneInByteseq(v[i:])
 		if c == utf8.RuneError && size == 1 {
 			if start < i {
-				w.rawStr(v[start:i])
+				w.Buf = append(w.Buf, v[start:i]...)
 			}
 			w.rawStr(`\ufffd`)
 			i++
@@ -186,7 +194,7 @@ func (w *Writer) strEscape(i int, v string, valLen int) {
 		// See http://timelessrepo.com/json-isnt-a-javascript-subset for discussion.
 		if c == '\u2028' || c == '\u2029' {
 			if start < i {
-				w.rawStr(v[start:i])
+				w.Buf = append(w.Buf, v[start:i]...)
 			}
 			w.rawStr(`\u202`)
 			w.byte(hexChars[c&0xF])
@@ -197,7 +205,7 @@ func (w *Writer) strEscape(i int, v string, valLen int) {
 		i += size
 	}
 	if start < len(v) {
-		w.rawStr(v[start:])
+		w.Buf = append(w.Buf, v[start:]...)
 	}
 	w.byte('"')
 }
@@ -223,6 +231,18 @@ var safeSet = [256]byte{
 // Use StrEscape to escape html, this is default for encoding/json and
 // should be used by default for untrusted strings.
 func (w *Writer) Str(v string) {
+	writeStr(w, v)
+}
+
+// ByteStr encodes string without html escaping.
+//
+// Use ByteStrEscape to escape html, this is default for encoding/json and
+// should be used by default for untrusted strings.
+func (w *Writer) ByteStr(v []byte) {
+	writeStr(w, v)
+}
+
+func writeStr[T byteseq](w *Writer, v T) {
 	w.Buf = append(w.Buf, '"')
 
 	// Fast path, without utf8 and escape support.
@@ -243,10 +263,10 @@ func (w *Writer) Str(v string) {
 	}
 slow:
 	w.Buf = append(w.Buf, v[:i]...)
-	w.strSlow(v[i:])
+	strSlow[T](w, v[i:])
 }
 
-func (w *Writer) strSlow(v string) {
+func strSlow[T byteseq](w *Writer, v T) {
 	var i, start int
 	// for the remaining parts, we process them char by char
 	for i < len(v) {
@@ -256,7 +276,7 @@ func (w *Writer) strSlow(v string) {
 			continue
 		}
 		if start < i {
-			w.rawStr(v[start:i])
+			w.Buf = append(w.Buf, v[start:i]...)
 		}
 		switch b {
 		case '\\', '"':
@@ -281,7 +301,7 @@ func (w *Writer) strSlow(v string) {
 		continue
 	}
 	if start < len(v) {
-		w.rawStr(v[start:])
+		w.Buf = append(w.Buf, v[start:]...)
 	}
 	w.byte('"')
 }
